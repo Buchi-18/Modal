436d3a2dfabeb8b74684e4cce58e4f521f8ec6ae
Buchi-18 <taka25803690@gmail.com>
2023-01-25 08:14:22 +0900

    add new images&videos

--- js/vanilla.js
+++ js/vanilla.js
@@ -1,7 +1,53 @@
-"use strict"
-const modal = document.getElementById("modal")
-const contentCell = document.querySelector(".content-cell")
+"use strict";
+const videos = [
+  {
+    dataId: 0,
+    videoUrl: "./video/video_b_640*360.mp4",
+    imgUrl: "./img/thumbnail-b.jpg",
+    alt: "サムネイル00",
+    title: "サムネイル00",
+  },
+  {
+    dataId: 1,
+    videoUrl: "./video/video_g_640*360.mp4",
+    imgUrl: "./img/thumbnail-g.jpg",
+    alt: "サムネイル01",
+    title: "サムネイル01",
+  },
+];
 
-contentCell.addEventListener("click",function(){
-  modal.classList.add("open")
-})
+const contentsWrap = document.getElementById("contentsWrap");
+const modal = document.getElementById("modal");
+const closeBtn = document.getElementById("closeBtn");
+const videoLink = document.getElementById("videoLink");
+const imgLink = document.getElementById("imgLink");
+
+// 配列videosのデータの数、サムネイル（li.content-cell）を追加
+videos.forEach(function (video) {
+  const { dataId, imgUrl, alt, title } = video;
+  contentsWrap.innerHTML += `
+  <li class="content-cell" data-id=${dataId}>
+          <div>
+            <img src=${imgUrl} alt=${alt} title=${title}/>
+          </div>
+        </li>
+  `;
+});
+
+// クリックしたサムネイルのdataIdから、表示するデータを取得しモーダルを表示
+document.querySelectorAll(".content-cell").forEach(function (cell) {
+  cell.addEventListener("click", function (e) {
+    modal.classList.add("open");
+    const id = cell.dataset.id;
+    const { videoUrl, imgUrl, alt, title } = videos[id];
+    videoLink.setAttribute("href", videoUrl);
+    imgLink.setAttribute("src", imgUrl);
+    imgLink.setAttribute("alt", alt);
+    imgLink.setAttribute("alt", title);
+  });
+});
+
+// クローズボタンクリックでモーダルを閉じる
+closeBtn.addEventListener("click", function () {
+  modal.classList.remove("open");
+});
